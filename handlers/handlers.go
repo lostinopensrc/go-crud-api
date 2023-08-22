e08f971d704fbaa37f8f3d811f243effeb4eb28e
lostinopensrc <lostinopensrc@gmail.com>
2023-08-22 10:18:36 +0530

    added Update Movie API Code

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -43,3 +43,18 @@ func Create_Movie(w http.ResponseWriter, r *http.Request) {
 	schema.Movies = append(schema.Movies, movie)
 	json.NewEncoder(w).Encode(movie)
 }
+
+func Update_Movie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range schema.Movies {
+		if item.Id == params["id"] {
+			schema.Movies = append(schema.Movies[:index], schema.Movies[index+1:]...) // delete the movie on base of id
+			var movie schema.Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)   // decode json encoded body sent from Client using PUT Method
+			schema.Movies = append(schema.Movies, movie) // update the movie sent in Body of request.
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
